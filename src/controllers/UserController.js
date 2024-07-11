58ef8b80dc2109a1bc72a2107e4ae8780acb045c
samuelribeiroo <samuelribeirodev@gmail.com>
2024-07-11 18:35:11 -0300

    add: UserController function

---
+++ src/controllers/UserController.js
@@ -0,0 +1,13 @@
+const users = require("../mockup/users");
+
+module.exports = {
+  listAllUsers(request, response) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(users));
+  },
+
+  errorHandling(request, response) {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  },
+};
