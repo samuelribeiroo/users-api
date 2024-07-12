7e548577ad0a05f2e39d2163a8e4832fa9851cd7
samuelribeiroo <samuelribeirodev@gmail.com>
2024-07-12 16:42:01 -0300

    refactor: route 404 sugar syntax applied find method

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 const http = require("http");
-const users = require("./mockup/users");
-const routes = require("./routes");
-const UserController = require("./controllers/UserController");
 const log = console.log;
+const routes = require("./routes");
+const { isInvalidRequest } = require("./controllers/UserController");
 
 const port = 9090;
 
 const server = http.createServer((request, response) => {
   log(`Request Method: ${request.method} and Request URL ${request.url}`);
 
-  const route = routes.find((routeObj) => {
-    return (routeObj.endpoint === request.url && routeObj.method === request.method);
-  });
+  const route = routes.find(routeObj => routeObj.endpoint === request.url && routeObj.method === request.method);
 
   if (route) {
     route.handler(request, response);
   } else {
-    UserController.errorHandling(request, response)
-  }
-  
+    isInvalidRequest(request, response)
+  }  
 });
 
 server.listen(port, console.log(`Server is running at: http://localhost:${port}`));
